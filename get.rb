6239f71aadd41b82e23a0c359eb3cf8401273648
Martin Stabenfeldt <Martin.Stabenfeldt@skatteetaten.no>
2021-03-05 08:55:05 +0100

    The main program

--- get.rb
+++ get.rb
@@ -1,5 +1,3 @@
-require 'net/http'
-require 'json'
 require "httparty"
 require "pp"
 
@@ -7,24 +5,14 @@ require "pp"
 @orders_url      = "https://esi.evetech.net/latest/markets/10000002/orders/?datasource=tranquility&order_type=all&page=1"
 @lookup_name_url = "https://esi.evetech.net/latest/universe/names/?datasource=tranquility"
 
-
-# curl -X GET "https://esi.evetech.net/latest/markets/10000002/orders/?datasource=tranquility&order_type=all&page=1" 
 def get_orders
-	uri = URI(@orders_url)
-	Net::HTTP.start(uri.host, uri.port,
-	  :use_ssl => uri.scheme == 'https') do |http|
-	  request = Net::HTTP::Get.new uri
-	
-	  response = http.request request # Net::HTTPResponse object
-    JSON.parse( response.read_body )
-	end
+  HTTParty.get(@orders_url)
 end
 
 def extract_key(data, key)
-    data.map { |t| t[key] }
+  data.map { |t| t[key] }
 end
 
-# Returns product name.
 def get_names(type_ids)
   # Limit the set to 10 
   type_ids = type_ids.shift(10)
@@ -47,5 +35,5 @@ orders = get_orders
 type_ids = extract_key(orders, "type_id")
 
 # Fetch and print names for the type_ids
-puts get_names(type_ids)
+pp get_names(type_ids)
 
